d43ed10b2eea78579cdcce6b66c05e2b5b4e6a15
Muhammad Haider <mohammadhaider344@gmail.com>
2023-01-05 14:03:39 +0500

    Updates

---
+++ precache-manifest.1781dae87ca9fdcdb672c136c26acff8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c74e54cf4cf9c8ec27c3c5aae88be2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6aa7accd8007b9ccc79",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a1b52bd8b5e8e96b4d87",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "e429fd0b55c99596b3fc",
+    "url": "/static/css/main.11bc0d05.chunk.css"
+  },
+  {
+    "revision": "3c36112db91d4b4df841",
+    "url": "/static/js/2.ff538662.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.ff538662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6aa7accd8007b9ccc79",
+    "url": "/static/js/3.109f519b.chunk.js"
+  },
+  {
+    "revision": "a1b52bd8b5e8e96b4d87",
+    "url": "/static/js/4.23a9cb26.chunk.js"
+  },
+  {
+    "revision": "e429fd0b55c99596b3fc",
+    "url": "/static/js/main.c96ae4f3.chunk.js"
+  },
+  {
+    "revision": "71e6c3b60601fe1298d8",
+    "url": "/static/js/runtime-main.44bf8a87.js"
+  },
+  {
+    "revision": "00678a6ec2b4904205b04aaab64641d6",
+    "url": "/static/media/360technolabLogo.00678a6e.webp"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1806a92e78158261e09ac903a9d81231",
+    "url": "/static/media/aptech2.1806a92e.png"
+  },
+  {
+    "revision": "a83dd50ff83e35f21675f9c3fbbbe6d8",
+    "url": "/static/media/barechatsLogo.a83dd50f.webp"
+  },
+  {
+    "revision": "aa85464b4b37a5ce3ca8e1fa25b9e6b7",
+    "url": "/static/media/bright.aa85464b.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "0330409f8dcb9d1ee2c494fe6f6e2360",
+    "url": "/static/media/riotLogo.0330409f.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
